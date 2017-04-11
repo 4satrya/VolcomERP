78cd8974d210af551cee8ce23c70c9b4dc52c57a
Satrya Anas <catur.satrya@gmail.com>
2017-04-11 10:38:08 +0800

    slip new

--- VolcomInventory/FormSalesDelOrderSlip.vb
+++ VolcomInventory/FormSalesDelOrderSlip.vb
@@ -106,8 +106,44 @@ Public Class FormSalesDelOrderSlip
 
 
     Sub viewSalesDelOrder()
-        Dim query_c As ClassSalesDelOrder = New ClassSalesDelOrder()
-        Dim query As String = query_c.queryMain("AND a.id_store_contact_to='" + id_store_contact_to + "' AND a.id_comp_contact_from='" + id_comp_contact_from + "' AND a.id_report_status=1 AND a.last_update_by=" + id_user + " AND ISNULL(sdm.id_pl_sales_order_del_slip) ", "1")
+        Dim query As String = "SELECT a.id_pl_sales_order_del, a.id_sales_order, a.id_store_contact_to, (d.id_comp) AS `id_store`,(d.comp_name) AS store_name_to, (d.comp_number) AS store_number_to, CONCAT(d.comp_number, ' - ', d.comp_name) AS `store`, (d.address_primary) AS store_address_to, d.id_so_type, a.id_report_status, f.report_status, "
+        query += "a.pl_sales_order_del_note, a.pl_sales_order_del_date, DATE_FORMAT(a.pl_sales_order_del_date,'%Y-%m-%d') AS pl_sales_order_del_datex, a.pl_sales_order_del_number, b.sales_order_number, "
+        query += "DATE_FORMAT(a.pl_sales_order_del_date,'%d %M %Y') AS pl_sales_order_del_date, a.id_comp_contact_from,(wh.id_comp) AS `id_wh`, (wh.comp_number) AS `wh_number`,(wh.comp_name) AS `wh_name`, CONCAT(wh.comp_number, ' - ', wh.comp_name) AS `wh`, a.id_wh_drawer, drw.wh_drawer_code, drw.wh_drawer, cat.id_so_status, cat.so_status, "
+        query += "a.last_update, getUserEmp(a.last_update_by, 1) AS `last_user`, ('No') AS `is_select`, IFNULL(det.`total`,0) AS `total`, rmg.`total_remaining`  "
+        query += "FROM tb_pl_sales_order_del a "
+        query += "INNER JOIN tb_sales_order b ON a.id_sales_order = b.id_sales_order "
+        query += "INNER JOIN tb_m_comp_contact c ON c.id_comp_contact = a.id_store_contact_to "
+        query += "INNER JOIN tb_m_comp d ON c.id_comp = d.id_comp "
+        query += "INNER JOIN tb_lookup_report_status f ON f.id_report_status = a.id_report_status "
+        query += "INNER JOIN tb_m_comp_contact wh_cont ON wh_cont.id_comp_contact = a.id_comp_contact_from "
+        query += "INNER JOIN tb_m_comp wh ON wh.id_comp = wh_cont.id_comp "
+        query += "INNER JOIN tb_lookup_so_status cat ON cat.id_so_status = b.id_so_status "
+        query += "LEFT JOIN tb_m_wh_drawer drw ON drw.id_wh_drawer = a.id_wh_drawer "
+        query += "LEFT JOIN( "
+        query += "SELECT del.id_pl_sales_order_del, SUM(det.pl_sales_order_del_det_qty) AS `total` "
+        query += "FROM tb_pl_sales_order_del del "
+        query += "INNER JOIN tb_pl_sales_order_del_det det ON del.id_pl_sales_order_del = det.id_pl_sales_order_del "
+        query += "GROUP BY del.id_pl_sales_order_del "
+        query += ") det ON det.id_pl_sales_order_del = a.id_pl_sales_order_del "
+        query += "LEFT JOIN ( "
+        query += "SELECT so.id_sales_order, SUM(so_det.sales_order_det_qty) AS `total_so`, del.`total_del`, "
+        query += "(SUM(so_det.sales_order_det_qty)-IFNULL(del.`total_del`,0)) AS `total_remaining` "
+        query += "FROM tb_sales_order so "
+        query += "INNER JOIN tb_sales_order_det so_det ON so_det.id_sales_order = so.id_sales_order "
+        query += "LEFT JOIN ( "
+        query += "SELECT del.id_sales_order, SUM(del_det.pl_sales_order_del_det_qty) AS `total_del` "
+        query += "FROM tb_pl_sales_order_del del "
+        query += "INNER JOIN tb_pl_sales_order_del_det del_det ON del_det.id_pl_sales_order_del=del.id_pl_sales_order_del "
+        query += "WHERE del.id_report_status!=5 "
+        query += "GROUP BY del.id_sales_order "
+        query += ") del ON del.id_sales_order = so.id_sales_order "
+        query += "GROUP BY so.id_sales_order "
+        query += ") rmg ON rmg.id_sales_order = a.id_sales_order "
+        query += "Left Join tb_pl_sales_order_del_slip_det sd ON sd.id_pl_sales_order_del = a.id_pl_sales_order_del "
+        query += "Left Join tb_pl_sales_order_del_slip sdm On sdm.id_pl_sales_order_del_slip = sd.id_pl_sales_order_del_slip And sdm.id_report_status!=5 "
+        query += "WHERE a.id_pl_sales_order_del>0 "
+        query += "AND a.id_store_contact_to='" + id_store_contact_to + "' AND a.id_comp_contact_from='" + id_comp_contact_from + "' AND a.id_report_status=1 AND a.last_update_by=" + id_user + " AND ISNULL(sdm.id_pl_sales_order_del_slip) "
+        query += "ORDER BY a.id_pl_sales_order_del ASC "
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
         GCSalesDelOrder.DataSource = data
     End Sub

d373e61c953eefecf86d957f3bf0d71ad75683f5
Putu Septian Primadewa <iansaviour@gmail.com>
2020-04-27 15:14:21 +0800

    add check

--- VolcomInventory/FormFabricTypeDet.vb
+++ VolcomInventory/FormFabricTypeDet.vb
@@ -38,20 +38,28 @@
         If age = "" Or age = "0" Or fabric_type = "" Or fabrication = "" Then
             warningCustom("Please fill all field")
         Else
-            If id = "-1" Then 'new
-                Dim q As String = "INSERT INTO tb_m_fabric_type(fabric_type_name,fabrication,age) VALUES('" & fabric_type & "','" & fabrication & "','" & age & "')"
-                execute_non_query(q, True, "", "", "", "")
+            'check ada yang sama
+            Dim qc As String = "SELECT * FROM tb_m_fabric_type WHERE fabric_type_name='" & fabric_type & "' OR fabrication='" & fabrication & "'"
+            Dim dtc As DataTable = execute_query(qc, -1, True, "", "", "", "")
 
-                FormFabricType.load_list()
-                infoCustom("Fabric type saved")
-                Close()
-            Else 'edit
-                Dim q As String = "UPDATE tb_m_fabric_type SET fabric_type_name='" & fabric_type & "',fabrication='" & fabrication & "',age='" & age & "' WHERE id_fabric_type='" & id & "'"
-                execute_non_query(q, True, "", "", "", "")
+            If dtc.Rows.Count > 0 Then
+                warningCustom("Fabric type/fabrication already registered on list.")
+            Else
+                If id = "-1" Then 'new
+                    Dim q As String = "INSERT INTO tb_m_fabric_type(fabric_type_name,fabrication,age) VALUES('" & fabric_type & "','" & fabrication & "','" & age & "')"
+                    execute_non_query(q, True, "", "", "", "")
 
-                FormFabricType.load_list()
-                infoCustom("Fabric type saved")
-                Close()
+                    FormFabricType.load_list()
+                    infoCustom("Fabric type saved")
+                    Close()
+                Else 'edit
+                    Dim q As String = "UPDATE tb_m_fabric_type SET fabric_type_name='" & fabric_type & "',fabrication='" & fabrication & "',age='" & age & "' WHERE id_fabric_type='" & id & "'"
+                    execute_non_query(q, True, "", "", "", "")
+
+                    FormFabricType.load_list()
+                    infoCustom("Fabric type saved")
+                    Close()
+                End If
             End If
         End If
     End Sub

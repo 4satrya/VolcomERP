c70085453a11d34f198aaad57681b9cd5d71991a
Putu Septian Primadewa <iansaviour@gmail.com>
2020-10-30 12:20:54 +0800

    additional cost

--- VolcomInventory/FormAdditionalCost.Designer.vb
+++ VolcomInventory/FormAdditionalCost.Designer.vb
@@ -19,62 +19,117 @@ Partial Class FormAdditionalCost
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormAdditionalCost))
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
         Me.GCAdditionalCost = New DevExpress.XtraGrid.GridControl()
         Me.GVAdditionalCost = New DevExpress.XtraGrid.Views.Grid.GridView()
         Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn5 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn6 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn7 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.BRefresh = New DevExpress.XtraEditors.SimpleButton()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
         CType(Me.GCAdditionalCost, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GVAdditionalCost, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
         '
+        Me.PanelControl1.Controls.Add(Me.BRefresh)
         Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
         Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
         Me.PanelControl1.Name = "PanelControl1"
-        Me.PanelControl1.Size = New System.Drawing.Size(1067, 42)
+        Me.PanelControl1.Size = New System.Drawing.Size(1067, 46)
         Me.PanelControl1.TabIndex = 0
         '
         'GCAdditionalCost
         '
         Me.GCAdditionalCost.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GCAdditionalCost.Location = New System.Drawing.Point(0, 42)
+        Me.GCAdditionalCost.Location = New System.Drawing.Point(0, 46)
         Me.GCAdditionalCost.MainView = Me.GVAdditionalCost
         Me.GCAdditionalCost.Name = "GCAdditionalCost"
-        Me.GCAdditionalCost.Size = New System.Drawing.Size(1067, 530)
+        Me.GCAdditionalCost.Size = New System.Drawing.Size(1067, 526)
         Me.GCAdditionalCost.TabIndex = 1
         Me.GCAdditionalCost.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVAdditionalCost})
         '
         'GVAdditionalCost
         '
-        Me.GVAdditionalCost.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1, Me.GridColumn2, Me.GridColumn3})
+        Me.GVAdditionalCost.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1, Me.GridColumn2, Me.GridColumn3, Me.GridColumn4, Me.GridColumn5, Me.GridColumn6, Me.GridColumn7})
         Me.GVAdditionalCost.GridControl = Me.GCAdditionalCost
         Me.GVAdditionalCost.Name = "GVAdditionalCost"
         Me.GVAdditionalCost.OptionsView.ShowGroupPanel = False
         '
         'GridColumn1
         '
-        Me.GridColumn1.Caption = "GridColumn1"
+        Me.GridColumn1.Caption = "ID PPs"
+        Me.GridColumn1.FieldName = "id_additional_cost_pps"
         Me.GridColumn1.Name = "GridColumn1"
-        Me.GridColumn1.Visible = True
-        Me.GridColumn1.VisibleIndex = 0
         '
         'GridColumn2
         '
-        Me.GridColumn2.Caption = "GridColumn2"
+        Me.GridColumn2.Caption = "Number"
+        Me.GridColumn2.FieldName = "number"
         Me.GridColumn2.Name = "GridColumn2"
         Me.GridColumn2.Visible = True
-        Me.GridColumn2.VisibleIndex = 1
+        Me.GridColumn2.VisibleIndex = 0
         '
         'GridColumn3
         '
-        Me.GridColumn3.Caption = "GridColumn3"
+        Me.GridColumn3.Caption = "Created By"
+        Me.GridColumn3.FieldName = "created_by"
         Me.GridColumn3.Name = "GridColumn3"
         Me.GridColumn3.Visible = True
-        Me.GridColumn3.VisibleIndex = 2
+        Me.GridColumn3.VisibleIndex = 1
+        '
+        'GridColumn4
+        '
+        Me.GridColumn4.Caption = "Created Date"
+        Me.GridColumn4.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.GridColumn4.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.GridColumn4.FieldName = "created_date"
+        Me.GridColumn4.Name = "GridColumn4"
+        Me.GridColumn4.Visible = True
+        Me.GridColumn4.VisibleIndex = 2
+        '
+        'GridColumn5
+        '
+        Me.GridColumn5.Caption = "Update By"
+        Me.GridColumn5.FieldName = "update_by"
+        Me.GridColumn5.Name = "GridColumn5"
+        Me.GridColumn5.Visible = True
+        Me.GridColumn5.VisibleIndex = 3
+        '
+        'GridColumn6
+        '
+        Me.GridColumn6.Caption = "Update Date"
+        Me.GridColumn6.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.GridColumn6.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.GridColumn6.FieldName = "update_date"
+        Me.GridColumn6.Name = "GridColumn6"
+        Me.GridColumn6.Visible = True
+        Me.GridColumn6.VisibleIndex = 4
+        '
+        'GridColumn7
+        '
+        Me.GridColumn7.Caption = "Status"
+        Me.GridColumn7.FieldName = "report_status"
+        Me.GridColumn7.Name = "GridColumn7"
+        Me.GridColumn7.Visible = True
+        Me.GridColumn7.VisibleIndex = 5
+        '
+        'BRefresh
+        '
+        Me.BRefresh.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BRefresh.Image = CType(resources.GetObject("BRefresh.Image"), System.Drawing.Image)
+        Me.BRefresh.Location = New System.Drawing.Point(946, 2)
+        Me.BRefresh.Name = "BRefresh"
+        Me.BRefresh.Size = New System.Drawing.Size(119, 42)
+        Me.BRefresh.TabIndex = 0
+        Me.BRefresh.Text = "Refresh"
         '
         'FormAdditionalCost
         '
@@ -90,6 +145,7 @@ Partial Class FormAdditionalCost
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Additional Cost"
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
         CType(Me.GCAdditionalCost, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GVAdditionalCost, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -102,4 +158,9 @@ Partial Class FormAdditionalCost
     Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn5 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn6 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn7 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents BRefresh As DevExpress.XtraEditors.SimpleButton
 End Class
--- VolcomInventory/FormAdditionalCost.vb
+++ VolcomInventory/FormAdditionalCost.vb
@@ -1,9 +1,53 @@
 ﻿Public Class FormAdditionalCost
+    Dim bnew_active As String = "1"
+    Dim bedit_active As String = "1"
+    Dim bdel_active As String = "1"
+
     Private Sub FormAdditionalCost_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
     Sub load_view()
+        Dim q As String = "SELECT pps.id_additional_cost_pps,pps.number,pps.created_date,pps.update_date,emp_created.employee_name AS created_by,emp_upd.employee_name AS update_by,sts.id_report_status,sts.report_status
+FROM `tb_additional_cost_pps` pps
+INNER JOIN tb_m_user usr_created ON usr_created.id_user=pps.created_by
+INNER JOIN tb_m_employee emp_created ON usr_created.id_employee=emp_created.id_employee
+INNER JOIN tb_m_user usr_upd ON usr_upd.id_user=pps.update_by
+INNER JOIN tb_m_employee emp_upd ON usr_upd.id_employee=emp_upd.id_employee
+INNER JOIN tb_lookup_report_status sts ON sts.id_report_status=pps.id_report_status"
+        Dim dt As DataTable = execute_query(q, -1, True, "", "", "", "")
+        GCAdditionalCost.DataSource = dt
+        GVAdditionalCost.BestFitColumns()
+    End Sub
 
+    Private Sub BRefresh_Click(sender As Object, e As EventArgs) Handles BRefresh.Click
+        load_view()
+    End Sub
+
+    Private Sub FormAdditionalCost_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        check_menu()
+    End Sub
+
+    Private Sub FormAdditionalCost_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
+
+    Sub check_menu()
+        If GVAdditionalCost.RowCount < 1 Then
+            'hide all except new
+            bnew_active = "1"
+            bedit_active = "0"
+            bdel_active = "0"
+            checkFormAccess(Name)
+            button_main(bnew_active, bedit_active, bdel_active)
+        Else
+            'show all
+            bnew_active = "1"
+            bedit_active = "1"
+            bdel_active = "1"
+            checkFormAccess(Name)
+            button_main(bnew_active, bedit_active, bdel_active)
+        End If
     End Sub
 End Class
